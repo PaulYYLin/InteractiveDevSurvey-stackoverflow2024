21a7f5817c2c7f038005dc60c47ae131074aa4aa
Paul <Paul.YY.Lin@BenQ.com>
2025-02-07 01:35:04 -0700

    compress handle

--- utils/get_data.py
+++ utils/get_data.py
@@ -1,19 +1,26 @@
 import pandas as pd
-import gzip
+import zipfile
 from functools import lru_cache
+import streamlit as st
 
 class GetData:
     def __init__(self):
-        # 读取压缩的 CSV 文件
-        with gzip.open('survey_results_public.csv.zip', 'rt') as f:
-            self.data = pd.read_csv(
-                f,
-                usecols=lambda x: x in [
-                    'ResponseId', 'Age', 'Employment', 'MainBranch',
-                    'EdLevel', 'AISelect', 'AISent', 'AIBen',
-                    'AIToolCurrently Using'
-                ]
-            )
+        @st.cache_data
+        def load_data():
+            with zipfile.ZipFile('survey_results_public.csv.zip', 'r') as zip_ref:
+                # 假设 ZIP 文件中只有一个 CSV 文件
+                csv_filename = zip_ref.namelist()[0]
+                with zip_ref.open(csv_filename) as f:
+                    return pd.read_csv(
+                        f,
+                        usecols=lambda x: x in [
+                            'ResponseId', 'Age', 'Employment', 'MainBranch',
+                            'EdLevel', 'AISelect', 'AISent', 'AIBen',
+                            'AIToolCurrently Using'
+                        ]
+                    )
+        
+        self.data = load_data()
         
     @lru_cache(maxsize=1)
     def get_BI(self):
